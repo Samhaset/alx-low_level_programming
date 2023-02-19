22de4f29a4e578c9820103fa827c74101eaee4d7
Samuel <samuelnega194@gmail.com>
2023-02-19 12:19:28 -0800

    Software is eating the World

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -18,15 +18,15 @@ int main(void)
 		for (p = d + 1; p <= '9'; p++)
 		{
 
-			for (q = p+1; q <='9'; q++)
+			for (q = p + 1; q <= '9'; q++)
 			{
 				if ((p != d) != q)
 						{
 						putchar(d);
 						putchar(p);
 						putchar(q);
-						if (d == '7' && p =='8')
-						continue;
+						if (d == '7' && p == '8')
+							continue;
 						putchar(',');
 						putchar(' ');
 						}
