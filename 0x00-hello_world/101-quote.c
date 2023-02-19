8af0752b9e4c7cced63ce7bc1b7fc16ba63e75e0
Samuel <samuelnega194@gmail.com>
2023-02-19 12:58:35 -0800

    Intel

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <unistd.h>
-
-/**main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
- *  followed by a new line, to the standard error.
- *  Return: Always 1
+/**
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * followed by a newline, to the standard error.
+ * Return: Always 1.
  */
+
 
 int main(void)
 
 {
-	write(2, "and that piece of art is useful\" -dora kopar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" -Dora korpar, 2015-10-19\n", 59);
 	return (1);
 }
