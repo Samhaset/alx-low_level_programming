1af6ff95fd98c602ee1938d1321366c7b0eb06d6
Samuel <samuelnega194@gmail.com>
2023-02-19 12:32:19 -0800

    you have to be a genius to understand the simplicity

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
-#include <stdio.h>
 #include <unistd.h>
-/**
- * main - A c program that prints a line to standard error
- * Return: 1 (Great)
+
+/**main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ *  followed by a new line, to the standard error.
+ *  Return: Always 1
  */
+
 int main(void)
+
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	write(2, "and that piece of art is useful\" -dora kopar, 2015-10-19\n", 59);
 	return (1);
 }
