22f0273ab5744f6de151358fb1e292487d564b8d
Samuel <samuelnega194@gmail.com>
2023-02-23 13:23:37 -0800

    The numbers speak for themselves

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,1 +1,19 @@
+#include "main.h"
 
+/**
+ * print_numbers - print the numbers scince 0 up to 9
+ * Return: the numbers scince 0 up to 9
+ */
+
+void print numbers(void)
+
+{
+
+int x;
+
+for (x = 0; x <= 9; x++)
+{
+_putchar(x + '0');
+}
+_putchar('\n');
+}
