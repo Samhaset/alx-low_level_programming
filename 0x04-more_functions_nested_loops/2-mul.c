470ecb263e080e5ec0d26192f0daf15f2072ac05
Samuel <samuelnega194@gmail.com>
2023-02-23 13:00:23 -0800

    Collaboration is multiplication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print the numbers scince 0 up to 9
+ * Return: the numbers scince 0 up to 9
+ */
+
+void printnumbers(void)
+
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		_putchar(x + '0');
+	}
+	_putchar('\n');
+}
